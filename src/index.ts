24a6968d716490e9d5c27faa900845702aa44814
limsunhu3525 <limsunhu3525@gmail.com>
2026-07-14 11:17:01 +0900

    feat: connect monster runtime and optional debug panel

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import './localization';
+import { MonsterDebugPanel } from './debug/monsterDebugPanel';
+import { MonsterRuntimeController } from './monster/monsterRuntimeController';
 import options from './options';
 import { registerServiceWorker } from './registerServiceWorker';
 import { Roulette } from './roulette';
@@ -6,6 +8,18 @@ import { Roulette } from './roulette';
 registerServiceWorker();
 
 const roulette = new Roulette();
+const monsterRuntime = new MonsterRuntimeController();
+monsterRuntime.attach(roulette);
+
+const searchParams = new URLSearchParams(window.location.search);
+const debugEnabled = searchParams.get('debug') === '1';
+
+if (debugEnabled) {
+  const debugPanel = new MonsterDebugPanel(monsterRuntime);
+  debugPanel.start();
+  (window as any).monsterDebugPanel = debugPanel;
+}
 
 (window as any).roulette = roulette;
 (window as any).options = options;
+(window as any).monsterRuntime = monsterRuntime;
