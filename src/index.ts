0999035c58bd37256e697546dea92047d73494b2
limsunhu3525 <limsunhu3525@gmail.com>
2026-07-14 11:48:45 +0900

    feat: enable collision reaction VFX

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,8 @@
 import './localization';
 import { MonsterCollisionReactionSystem } from './collision/monsterCollisionReactionSystem';
 import { MonsterDebugPanel } from './debug/monsterDebugPanel';
+import { CollisionReactionVfxController } from './effects/collisionReactionVfxController';
+import type { GameObject } from './gameObject';
 import type { IPhysics } from './IPhysics';
 import { MonsterRuntimeController } from './monster/monsterRuntimeController';
-import options from './options';
-import { registerServiceWorker } from './registerServiceWorker';
-import { Roulette } from './roulette';
-import { MonsterSkillExecutor } from './skills/monsterSkillExecutor';
-import { TraitSelectionModal } from './ui/traitSelectionModal';
-
-const DEFAULT_GAME_SPEED = 0.65;
-
-registerServiceWorker();
-
-const roulette = new Roulette();
-roulette.setSpeed(DEFAULT_GAME_SPEED);
-
-const monsterRuntime = new MonsterRuntimeController();
-monsterRuntime.attach(roulette);
-
-const traitSelectionModal = new TraitSelectionModal(monsterRuntime);
-traitSelectionModal.mount();
-
-const getPhysics = () => (roulette as unknown as { physics?: IPhysics }).physics;
-
-const monsterSkillExecutor = new MonsterSkillExecutor(monsterRuntime, getPhysics);
-
-const monsterCollisionReactionSystem = new MonsterCollisionReactionSystem(monsterRuntime, getPhysics);
-monsterCollisionReactionSystem.attach(roulette);
-
-const searchParams = new URLSearchParams(window.location.search);
-const debugEnabled = searchParams.get('debug') === '1';
-
-if (debugEnabled) {
-  const debugPanel = new MonsterDebugPanel(monsterRuntime);
-  debugPanel.start();
-  (window as any).monsterDebugPanel = debugPanel;
-}
-
-(window as any).roulette = roulette;
-(window as any).options = options;
-(window as any).monsterRuntime = monsterRuntime;
-(window as any).monsterSkillExecutor = monsterSkillExecutor;
-(window as any).monsterCollisionReactionSystem = monsterCollisionReactionSystem;
-(window as any).traitSelectionModal = traitSelectionModal;
-(window as any).DEFAULT_GAME_SPEED = DEFAULT_GAME_SPEED;
+import options from './
