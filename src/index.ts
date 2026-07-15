d9772ce553c7cd2ae0a7473d730822753b37fa0f
limsunhu3525 <limsunhu3525@gmail.com>
2026-07-15 13:28:12 +0900

    feat: apply compact mobile layout overrides

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,7 @@ import { MonsterSkillExecutor } from './skills/monsterSkillExecutor';
 import { applyMonsterArcadeBranding } from './ui/branding';
 import { cleanupLegacyUi } from './ui/legacyUiCleanup';
 import './ui/mobileResponsive.scss';
+import './ui/mobileCompactOverrides.scss';
 import { MobileTraitSetupController } from './ui/mobileTraitSetupController';
 import { ResultModal } from './ui/resultModal';
 import './ui/selectionImageQuality.scss';
